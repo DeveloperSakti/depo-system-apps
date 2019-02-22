553662c3bfc76e44486b2eabf30225b71eb7a58c
mygetzu <mbenypangestu@gmail.com>
2019-02-22 17:05:34 +0700

    Update - view dashboard

--- resources/views/app/pages/dashboard/dashboard.blade.php
+++ resources/views/app/pages/dashboard/dashboard.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in our system!
+                    Welcome to our system. You are logged in!
                 </div>
             </div>
         </div>
