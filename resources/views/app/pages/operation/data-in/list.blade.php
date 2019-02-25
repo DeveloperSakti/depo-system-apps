dd102c74cac2b41236e8876833fd736255798aec
mygetzu <mbenypangestu@gmail.com>
2019-02-25 21:45:13 +0700

    Create - Form data in

--- resources/views/app/pages/operation/data-in/list.blade.php
+++ resources/views/app/pages/operation/data-in/list.blade.php
@@ -7,7 +7,7 @@
         <div class="section-header-breadcrumb">
           <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
           <div class="breadcrumb-item"><a href="#">Forms</a></div>
-          <div class="breadcrumb-item">Consignee Price Form</div>
+          <div class="breadcrumb-item">Consignee Price </div>
         </div>
       </div>
 
@@ -138,5 +138,5 @@
         </div>
         </form>
       </div>
-      
+
 @endsection
